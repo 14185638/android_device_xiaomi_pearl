54a873e1496bb1f48c4a94ca7d98451684c1569f
14185638 <98950213+14185638@users.noreply.github.com>
2026-04-04 15:46:12 +0800

    Add GMS

--- lineage_pearl.mk
+++ lineage_pearl.mk
@@ -10,7 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+ifeq ($(WITH_GMS),true)
+    $(call inherit-product, vendor/gms/products/gms.mk)
+endif
 # Inherit from pearl device
 $(call inherit-product, device/xiaomi/pearl/device.mk)
 
